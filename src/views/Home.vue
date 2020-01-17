29a7f3ec101f267e41b6cc511e16bcd301d92737
adam yusuf <adamyusuf3494@gmail.com>
2020-01-17 09:56:20 -0500

    Adam Yusuf added vuetify icon functionality and cleared up the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
+  <v-app>
+    <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is the homepage</h1>
+
   </div>
+</template>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
