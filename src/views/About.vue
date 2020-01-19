4ebdf034c1f07da9377a737efc0ef6a5bcbad3ca
adam yusuf <adamyusuf3494@gmail.com>
2020-01-18 20:05:37 -0500

    Adam Yusuf added content for empty pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-app>
+    <v-container class="pa-5" >
+      <v-layout row wrap justify-center class="px-5">
+        <v-flex xs11 sm11 md11 lg8 class="px-5">
+          <h2>My Passions & Interest</h2><br>
+              <h2>I am interested about web development and the ever changing web technologies.<br> My passion is building simple and clean user interfaces. My interest 
+              also include cloud computing and automation</h2>
+              <br><h2>I enjoy sports, both participating and watching. I support the Washington Wizards but have a soft spot for Toronto Raptors. I also support Manchester United</h2>
+              <br><h2>I enjoy gaming and building computers in my spare time</h2>
+              <br><h2>Do not hesitate to contact me</h2>
+        </v-flex>
+      </v-layout>
+        </v-container>
+    
+  </v-app>
 </template>
---
+++ src/views/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app>
+        <v-container class="pa-5" >
+      <v-layout row wrap justify-center class="px-5">
+        <v-flex xs8 sm8 md8  lg2>
+          <v-img alt="Vue logo" src="../assets/snakeProject.png" width="200px" />
+        </v-flex>
+        <v-flex xs8 sm8 md8 lg6 class="px-5">
+          <p class="text-center">
+        This project is a snake game that I build using vue framework. I incorporated firebase authentication to
+        access the game. Firebase was also used to implement a backend to record the users score and to create a leaderboard<br>
+        <strong>To use this project please log in</strong>
+      </p>
+      <v-btn  class="text-xs-center" href="#">Source Code</v-btn>
+        </v-flex>
+      </v-layout>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
