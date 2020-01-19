6518c25a78f89eb6074d744f4728ead7fe1c3375
adam yusuf <adamyusuf3494@gmail.com>
2020-01-18 20:35:26 -0500

    Adam Yusuf added footer but needs to figure out how to make it its own component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
   <v-app>
+    <footer />
       <Navbar v-show="$route.name !== 'home'"/>
+      
     <v-content>
       <router-view></router-view>
     </v-content>
+    
+    <v-footer>
+            <v-row
+            class="primary lighten-2  text-center black--text"
+        justify="center"
+        no-gutters
+      >
+           <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          color="black"
+          text
+          rounded
+          class="blue lighten-2 my-2"
+        >
+          <v-icon>{{ icon }}</v-icon>
+          
+        </v-btn> 
+        <v-col
+          class="primary lighten-2  text-center black--text"
+          cols="12"
+        >
+          <span>&copy;2020 — <strong>Adam Yusuf</strong></span>
+        </v-col>
+            </v-row>
+    </v-footer>
   </v-app>
 </template>
 
@@ -11,12 +39,16 @@
 import Navbar from '@/components/Navbar'
 export default {
   name: 'App',
-  components: { Navbar },
+  components: { Navbar  },
 
   
 
   data: () => ({
-    //
+    icons: [
+      'email',
+      'fab fa-linkedin',
+      'fab fa-github'
+    ],
   }),
 };
 </script>
